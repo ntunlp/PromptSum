128b8ed090c1ad7b8796eab1be3abe7f624bc48c
Mathieu <mathieu.ravaut@gmail.com>
2022-03-28 18:29:14 +0800

    base

--- hf_examples/guided_decoding.py
+++ hf_examples/guided_decoding.py
@@ -8,19 +8,22 @@ t5model = T5ForConditionalGeneration.from_pretrained(model_name, cache_dir=cache
 
 sequence = "my dog is under the table"
 sequence_tok = tokenizer(sequence, return_tensors="pt")
-labels = "have you"
-labels_tok = tokenizer(labels, return_tensors="pt")
 prompt = "summarize"
 prompt_tok = tokenizer(prompt, return_tensors="pt")
-print(sequence_tok["input_ids"].shape, sequence_tok["attention_mask"].shape, prompt_tok["input_ids"].shape, labels_tok["input_ids"].shape)
-outs = t5model.generate(
+labels = "have you"
+labels_tok = tokenizer(labels, return_tensors="pt")
+print(sequence_tok["input_ids"].shape, prompt_tok["input_ids"].shape, labels_tok["input_ids"].shape)
+
+labels_tok["input_ids"] = torch.cat((prompt_tok["input_ids"], labels_tok["input_ids"]), 0)
+# labels_tok["attention_mask"] = torch.cat((prompt_tok["attention_mask"], labels_tok["attention_mask"]), 0)
+
+outs = t5model.forward(
     input_ids = sequence_tok["input_ids"], 
     attention_mask = sequence_tok["attention_mask"],
-    #labels = labels_tok["input_ids"],
+    labels = labels_tok["input_ids"],
     #decoder_input_ids = labels_tok["input_ids"],
     #min_length = 20
 )
-print(outs)
-out = tokenizer.batch_decode(outs)
-print(out)
+logits = outs["logits"]
+print(logits.shape)
 
