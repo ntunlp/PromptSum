18cd7b7f764b9423610eecab95b1714f7dfd285c
qcw <qcwthu@126.com>
2022-04-12 16:07:29 +0800

    1

--- Summarization/runall.sh
+++ Summarization/runall.sh
@@ -1,42 +1,43 @@
-## 10-shot
-## train t5 tagger
-echo "start --few_shot 10 --train_t5_tagger"
-python main.py --few_shot 10 --train_t5_tagger
-echo "finish --few_shot 10 --train_t5_tagger"
-## oracle: target (T5 entity)
-echo "start --few_shot 10 --guidance_mode oracle"
-python main.py --few_shot 10 --guidance_mode oracle
-echo "finish --few_shot 10 --guidance_mode oracle"
-## T5 tagger (generate entities in summary)
-echo "start --few_shot 10 --guidance_mode nomral"
-python main.py --few_shot 10 --guidance_mode nomral
-echo "finish --few_shot 10 --guidance_mode nomral"
-
 ## 64-shot
 ## train t5 tagger
-echo "start --few_shot 64 --train_t5_tagger"
-python main.py --few_shot 64 --train_t5_tagger
-echo "finish --few_shot 64 --train_t5_tagger"
+echo "start --few_shot 64 --train_t5_tagger --if_spacy"
+#python main.py --few_shot 64 --train_t5_tagger --if_spacy
+echo "finish --few_shot 64 --train_t5_tagger --if_spacy"
 ## oracle: target (T5 entity)
-echo "start --few_shot 64 --guidance_mode oracle"
-python main.py --few_shot 64 --guidance_mode oracle
-echo "finish --few_shot 64 --guidance_mode oracle"
+echo "start --few_shot 64 --guidance_mode target"
+python main.py --few_shot 64 --guidance_mode target
+echo "finish --few_shot 64 --guidance_mode target"
 ## T5 tagger (generate entities in summary)
-echo "start --few_shot 64 --guidance_mode nomral"
-python main.py --few_shot 64 --guidance_mode nomral
-echo "finish --few_shot 64 --guidance_mode nomral"
+echo "start --few_shot 64 --guidance_mode input"
+python main.py --few_shot 64 --guidance_mode input
+echo "finish --few_shot 64 --guidance_mode input"
 
 
 ## 100-shot
 ## train t5 tagger
-echo "start --few_shot 100 --train_t5_tagger"
-python main.py --few_shot 100 --train_t5_tagger
-echo "finish --few_shot 100 --train_t5_tagger"
+echo "start --few_shot 100 --train_t5_tagger --if_spacy"
+python main.py --few_shot 100 --train_t5_tagger --if_spacy
+echo "finish --few_shot 100 --train_t5_tagger --if_spacy"
 ## oracle: target (T5 entity)
-echo "start --few_shot 100 --guidance_mode oracle"
-python main.py --few_shot 100 --guidance_mode oracle
-echo "finish --few_shot 100 --guidance_mode oracle"
+echo "start --few_shot 100 --guidance_mode target"
+#python main.py --few_shot 100 --guidance_mode target
+echo "finish --few_shot 100 --guidance_mode target"
 ## T5 tagger (generate entities in summary)
-echo "start --few_shot 100 --guidance_mode nomral"
-python main.py --few_shot 100 --guidance_mode nomral
-echo "finish --few_shot 100 --guidance_mode nomral"
+echo "start --few_shot 100 --guidance_mode input"
+python main.py --few_shot 100 --guidance_mode input
+echo "finish --few_shot 100 --guidance_mode input"
+
+
+## 10-shot
+## train t5 tagger
+echo "start --few_shot 10 --train_t5_tagger --if_spacy"
+python main.py --few_shot 10 --train_t5_tagger --if_spacy
+echo "finish --few_shot 10 --train_t5_tagger --if_spacy"
+## oracle: target (T5 entity)
+echo "start --few_shot 10 --guidance_mode target"
+#python main.py --few_shot 10 --guidance_mode target
+echo "finish --few_shot 10 --guidance_mode target"
+## T5 tagger (generate entities in summary)
+echo "start --few_shot 10 --guidance_mode input"
+python main.py --few_shot 10 --guidance_mode input
+echo "finish --few_shot 10 --guidance_mode input"
