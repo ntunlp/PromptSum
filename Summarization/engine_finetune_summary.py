437e0f280df2dcad81b3c2f54e1e063a635d28e0
ruochenzhao <rz973@nyu.edu>
2022-06-19 16:59:49 +0800

    pushed fix

--- Summarization/engine_finetune_summary.py
+++ Summarization/engine_finetune_summary.py
@@ -170,7 +170,7 @@ def train(tokenizer, model, train_dataset, valid_dataset, logger, args):
             "f1": 0.0
         }
         result_dict['epoch'] = args.max_epoch_summary
-        dooneeval(model, test_dataloader, scaler, result_dict, logger, i, args)
+        dooneeval(model, test_dataloader, scaler, result_dict, logger, args.max_epoch_summary, args)
     torch.cuda.empty_cache()
     del model, optimizer, scheduler, scaler, train_dataloader, valid_dataloader,
     gc.collect()
