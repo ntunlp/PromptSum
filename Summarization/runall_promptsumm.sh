db5b5dcdf553199d24ffb0a5aa6a6099e655f9af
Ravoxsg <mathieu.ravaut@gmail.com>
2022-06-19 18:04:07 +0800

    base

--- Summarization/runall_promptsumm.sh
+++ Summarization/runall_promptsumm.sh
@@ -2,48 +2,48 @@
 #pretrain_prompt_ckpt="/data/qin/PromptSumm/Summarization/t5_tagger_pretrained_ckpt_0520bak/bestckpt_prompt_114k"
 #pretrain_ckpt="/data/hailin/PromptSumm/t5_tagger_pretrained_ckpt/012_c_210k/bestckpt_full_model"
 #pretrain_prompt_ckpt="/data/hailin/PromptSumm/t5_tagger_pretrained_ckpt/012_c_210k/bestckpt_prompt"
-pretrain_ckpt="/data/hailin/PromptSumm/t5_tagger_pretrained_ckpt/012_cc_ent_v2_135k/bestckpt_full_model"
-pretrain_prompt_ckpt="/data/hailin/PromptSumm/t5_tagger_pretrained_ckpt/012_cc_ent_v2_135k/bestckpt_prompt"
+pretrain_ckpt="/data/hailin/PromptSumm/t5_tagger_pretrained_ckpt/012_cc_ent_v2_120k/012_cc_ent_v2_120k/bestckpt_full_model"
+pretrain_prompt_ckpt="/data/hailin/PromptSumm/t5_tagger_pretrained_ckpt/012_cc_ent_v2_120k/012_cc_ent_v2_120k/bestckpt_prompt"
 
 
 ## 10-shot
 echo "start 10-shot prompt-tune_entity"
-#python main.py --few_shot 10 --finetune_entity --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt
+python main.py --few_shot 10 --finetune_entity --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt --max_epoch_entity 60
 echo "end 10-shot prompt-tune_entity"
 
 echo "start 10-shot prompt-tune_summary"
-python main.py --few_shot 10 --finetune_summary --infer_val_entities --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt
-python main.py --few_shot 10 --finetune_summary --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt
+#python main.py --few_shot 10 --finetune_summary --infer_val_entities --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt
+python main.py --few_shot 10 --finetune_summary --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt --max_epoch_summary 60
 echo "end 10-shot prompt-tune_summary"
 
 echo "start 10-shot prompt-tune_summary oracle"
-python main.py --few_shot 10 --finetune_summary --guidance_mode target --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt
+#python main.py --few_shot 10 --finetune_summary --guidance_mode target --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt
 echo "end 10-shot prompt-tune_summary oracle"
 
 
 ## 64-shot
 echo "start 64-shot prompt-tune_entity"
-python main.py --few_shot 64 --finetune_entity --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt
+#python main.py --few_shot 64 --finetune_entity --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt
 echo "end 64-shot prompt-tune_entity"
 
 echo "start 64-shot prompt-tune_summary"
-python main.py --few_shot 64 --finetune_summary --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt 
+#python main.py --few_shot 64 --finetune_summary --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt 
 echo "end 64-shot prompt-tune_summary"
 
 echo "start 64-shot prompt-tune_summary oracle"
-python main.py --few_shot 64 --finetune_summary --guidance_mode target --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt
+#python main.py --few_shot 64 --finetune_summary --guidance_mode target --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt
 echo "end 64-shot prompt-tune_summary oracle"
 
 
 ## 100-shot
 echo "start 100-shot prompt-tune_entity"
-python main.py --few_shot 100 --finetune_entity --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt
+#python main.py --few_shot 100 --finetune_entity --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt
 echo "end 100-shot prompt-tune_entity"
 
 echo "start 100-shot prompt-tune_summary"
-python main.py --few_shot 100 --finetune_summary --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt 
+#python main.py --few_shot 100 --finetune_summary --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt 
 echo "end 100-shot prompt-tune_summary"
 
 echo "start 100-shot prompt-tune_summary oracle"
-python main.py --few_shot 100 --finetune_summary --guidance_mode target --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt
+#python main.py --few_shot 100 --finetune_summary --guidance_mode target --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt
 echo "end 100-shot prompt-tune_summary oracle"
