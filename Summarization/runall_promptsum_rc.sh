13020c74a341f64d48fbe7a47065ab7b3fab3ed5
ruochenzhao <rz973@nyu.edu>
2022-06-20 15:11:29 +0800

    rc running

--- Summarization/runall_promptsum_rc.sh
+++ Summarization/runall_promptsum_rc.sh
@@ -10,33 +10,25 @@ pretrain_ckpt="/data/hailin/PromptSumm/t5_tagger_pretrained_ckpt/012_cc_ent_v2_1
 pretrain_prompt_ckpt="/data/hailin/PromptSumm/t5_tagger_pretrained_ckpt/012_cc_ent_v2_120k/012_cc_ent_v2_120k/bestckpt_prompt"
 
 
-## 10-shot
-echo "start 10-shot prompt-tune_entity"
-# python main.py --few_shot 10 --finetune_entity --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt --dataset_name ccdv/cnn_dailymail
-echo "end 10-shot prompt-tune_entity"
+# ## 10-shot
+# echo "start 10-shot prompt-tune_entity"
+# # python main.py --few_shot 10 --finetune_entity --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt --dataset_name ccdv/cnn_dailymail
+# echo "end 10-shot prompt-tune_entity"
 
-echo "start 10-shot prompt-tune_summary"
+# echo "start 10-shot prompt-tune_summary"
+# # python main.py --few_shot 10 --finetune_summary --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt --max_epoch_summary 60 --full_testset --dataset_name ccdv/cnn_dailymail
 # python main.py --few_shot 10 --finetune_summary --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt --max_epoch_summary 60 --full_testset --dataset_name ccdv/cnn_dailymail
-python main.py --few_shot 10 --finetune_summary --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt --max_epoch_summary 0 --full_testset --dataset_name ccdv/cnn_dailymail
-echo "end 10-shot prompt-tune_summary"
-
-echo "start 10-shot prompt-tune_summary oracle"
-#python main.py --few_shot 10 --finetune_summary --infer_val_entities --guidance_mode target --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt
-echo "end 10-shot prompt-tune_summary oracle"
+# echo "end 10-shot prompt-tune_summary"
 
 
-# ## 64-shot
-# echo "start 64-shot prompt-tune_entity"
-# python main.py --few_shot 64 --finetune_entity --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt --dataset_name ccdv/cnn_dailymail
-# echo "end 64-shot prompt-tune_entity"
+## 64-shot
+echo "start 64-shot prompt-tune_entity"
+python main.py --few_shot 64 --finetune_entity --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt --dataset_name ccdv/cnn_dailymail
+echo "end 64-shot prompt-tune_entity"
 
-# echo "start 64-shot prompt-tune_summary"
-# python main.py --few_shot 64 --finetune_summary --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt --max_epoch_summary 60 --full_testset --dataset_name ccdv/cnn_dailymail
-# echo "end 64-shot prompt-tune_summary"
-
-# echo "start 64-shot prompt-tune_summary oracle"
-# #python main.py --few_shot 64 --finetune_summary --infer_val_entities --guidance_mode target --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt
-# echo "end 64-shot prompt-tune_summary oracle"
+echo "start 64-shot prompt-tune_summary"
+python main.py --few_shot 64 --finetune_summary --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt --max_epoch_summary 60 --full_testset --dataset_name ccdv/cnn_dailymail
+echo "end 64-shot prompt-tune_summary"
 
 
 # ## 100-shot
