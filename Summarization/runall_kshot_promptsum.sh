eb038089cb3ef6b1e9e0d6fa21d199bd48288d24
ruochenzhao <rz973@nyu.edu>
2022-08-11 22:44:33 +0800

    changed script for bug

--- Summarization/runall_kshot_promptsum.sh
+++ Summarization/runall_kshot_promptsum.sh
@@ -26,9 +26,9 @@ pretrain_ckpt="/data/hailin/PromptSumm/t5_tagger_pretrained_ckpt/014_c_340k/best
 pretrain_prompt_ckpt="/data/hailin/PromptSumm/t5_tagger_pretrained_ckpt/014_c_340k/bestckpt_prompt"
 ### k-shot
 echo "start k-shot prompt-tune_entity"
-CUDA_VISIBLE_DEVICES=$device python main.py --dataset_name $dataset --few_shot $k_shot --finetune_entity --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt --max_epoch_entity 60 --model_name google/pegasus-large --use_lm_adapted 0 --cache_path /data/ruochen/hf_models/pegasus-large/
+# CUDA_VISIBLE_DEVICES=$device python main.py --dataset_name $dataset --few_shot $k_shot --finetune_entity --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt --max_epoch_entity 60 --model_name google/pegasus-large --use_lm_adapted 0 --cache_path /data/ruochen/hf_models/pegasus-large/
 echo "end k-shot prompt-tune_entity"
 
-# echo "start k-shot prompt-tune_summary"
-# CUDA_VISIBLE_DEVICES=$device python main.py --dataset_name $dataset --few_shot $k_shot --finetune_summary --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt --max_epoch_summary 60 --model_name google/pegasus-large --use_lm_adapted 0 --cache_path /data/ruochen/hf_models/pegasus-large/
-# echo "end k-shot prompt-tune_summary"
+echo "start k-shot prompt-tune_summary"
+CUDA_VISIBLE_DEVICES=$device python main.py --model PegasusMixPrompt --dataset_name $dataset --few_shot $k_shot --finetune_summary --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt --max_epoch_summary 60 --model_name google/pegasus-large --use_lm_adapted 0 --cache_path /data/ruochen/hf_models/pegasus-large/
+echo "end k-shot prompt-tune_summary"
