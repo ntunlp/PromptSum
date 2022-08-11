6a5695d3ed95411d53ee7ad5c35bbc99b93d4f4f
Mathieu <mathieu.ravaut@gmail.com>
2022-08-11 17:20:38 +0200

    pegasus model default

--- Summarization/run_fullshot_promptsum.sh
+++ Summarization/run_fullshot_promptsum.sh
@@ -12,10 +12,10 @@ pretrain_prompt_ckpt="/data/hailin/PromptSumm/t5_tagger_pretrained_ckpt/014_c_34
 
 # entity
 echo "start full-shot prompt-tune_entity"
-CUDA_VISIBLE_DEVICES=$device python main_full_shot.py  --dataset_name $dataset --finetune_entity --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt --max_epoch_entity 5
+CUDA_VISIBLE_DEVICES=$device python --model PegasusMixPrompt main_full_shot.py  --dataset_name $dataset --finetune_entity --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt --max_epoch_entity 5 --model_name google/pegasus-large --use_lm_adapted 0 --cache_path /data/ruochen/hf_models/pegasus-large/
 echo "end full-shot prompt-tune_entity"
 
 # summary
 echo "start full-shot prompt-tune_summary"
-CUDA_VISIBLE_DEVICES=$device python main_full_shot.py  --dataset_name $dataset --finetune_summary --pretrain_ckpt $pretrain_ckpt  --pretrain_prompt_ckpt $pretrain_prompt_ckpt --max_epoch_summary 5
+CUDA_VISIBLE_DEVICES=$device python --model PegasusMixPrompt main_full_shot.py  --dataset_name $dataset --finetune_summary --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt --max_epoch_summary 5 --model_name google/pegasus-large --use_lm_adapted 0 --cache_path /data/ruochen/hf_models/pegasus-large/
 echo "end full-shot prompt-tune_summary"
--- Summarization/run_zeroshot_promptsum.sh
+++ Summarization/run_zeroshot_promptsum.sh
@@ -13,9 +13,9 @@ pretrain_prompt_ckpt="/data/hailin/PromptSumm/t5_tagger_pretrained_ckpt/014_c_34
 
 ### k-shot
 echo "start k-shot prompt-tune_entity"
-CUDA_VISIBLE_DEVICES=$device python main.py --dataset_name $dataset --few_shot $k_shot --num_seeds 1 --zero_shot --finetune_entity --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt --model_name google/pegasus-large --use_lm_adapted 0 --cache_path /data/ruochen/hf_models/pegasus-large/
+CUDA_VISIBLE_DEVICES=$device python main.py --model PegasusMixPrompt --dataset_name $dataset --few_shot $k_shot --num_seeds 1 --zero_shot --finetune_entity --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt --model_name google/pegasus-large --use_lm_adapted 0 --cache_path /data/ruochen/hf_models/pegasus-large/
 echo "end k-shot prompt-tune_entity"
 
 echo "start k-shot prompt-tune_summary"
-CUDA_VISIBLE_DEVICES=$device python main.py --dataset_name $dataset --few_shot $k_shot --num_seeds 1 --zero_shot --finetune_summary --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt --model_name google/pegasus-large --use_lm_adapted 0 --cache_path /data/ruochen/hf_models/pegasus-large/
+CUDA_VISIBLE_DEVICES=$device python main.py --model PegasusMixPrompt --dataset_name $dataset --few_shot $k_shot --num_seeds 1 --zero_shot --finetune_summary --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt --model_name google/pegasus-large --use_lm_adapted 0 --cache_path /data/ruochen/hf_models/pegasus-large/
 echo "end k-shot prompt-tune_summary"
