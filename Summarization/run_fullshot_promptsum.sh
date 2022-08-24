0b65e9eb3b6a913a30ace98b4b2606c0e8a96989
Mathieu <mathieu.ravaut@gmail.com>
2022-08-24 22:25:19 +0800

    test pipe for full-shot tuning

--- Summarization/run_fullshot_promptsum.sh
+++ Summarization/run_fullshot_promptsum.sh
@@ -1,6 +1,6 @@
 ### dataset
 dataset="xsum" # in ["xsum", "ccdv/cnn_dailymail"]
-device="0"
+device="1"
 
 ### backbone model
 ### T5-large backbone
@@ -10,12 +10,14 @@ device="0"
 pretrain_ckpt="/data/hailin/PromptSumm/t5_tagger_pretrained_ckpt/014_c_1070k/bestckpt_full_model"
 pretrain_prompt_ckpt="/data/hailin/PromptSumm/t5_tagger_pretrained_ckpt/014_c_1070k/bestckpt_prompt"
 
-# entity
+# train + val
 echo "start full-shot prompt-tune_entity"
-CUDA_VISIBLE_DEVICES=$device python main_full_shot.py --model PegasusMixPrompt --dataset_name $dataset --finetune_entity --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt --cache_path /data/mathieu/hf_models/pegasus-large/
-echo "end full-shot prompt-tune_entity"
-
-# summary
+#CUDA_VISIBLE_DEVICES=$device python main_full_shot.py --model PegasusMixPrompt --dataset_name $dataset --finetune_entity --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt --cache_path /data/mathieu/hf_models/pegasus-large/
 echo "start full-shot prompt-tune_summary"
-CUDA_VISIBLE_DEVICES=$device python main_full_shot.py --model PegasusMixPrompt --dataset_name $dataset --finetune_summary --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt --cache_path /data/mathieu/hf_models/pegasus-large/
-echo "end full-shot prompt-tune_summary"
+#CUDA_VISIBLE_DEVICES=$device python main_full_shot.py --model PegasusMixPrompt --dataset_name $dataset --finetune_summary --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt --cache_path /data/mathieu/hf_models/pegasus-large/
+
+# test
+echo "start full-shot prompt-tune_entity - TEST SET"
+CUDA_VISIBLE_DEVICES=$device python main_full_shot.py --model PegasusMixPrompt --dataset_name $dataset --full_test_set --finetune_entity --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt --max_epoch_entity 0 --cache_path /data/mathieu/hf_models/pegasus-large/
+echo "start full-shot prompt-tune_summary - TEST SET"
+CUDA_VISIBLE_DEVICES=$device python main_full_shot.py --model PegasusMixPrompt --dataset_name $dataset --full_test_set --finetune_summary --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt --max_epoch_summary 0 --cache_path /data/mathieu/hf_models/pegasus-large/
