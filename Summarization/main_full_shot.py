fixed small args bug + Pegasus scaling bug

Summarization/main_full_shot.py
@@ -123,6 +123,8 @@ def set_args():
     parser.add_argument("--adam_epsilon", dest="adam_epsilon", type=float,
                         default = 1e-8, help="adam epsilon")
     ##### pretraining
+    parser.add_argument("--optimizer_pretrain", dest="optimizer_pretrain", type=str,
+                        default="adafactor", help="optimizer for the pre-training")
     parser.add_argument("--lr_pretrain", dest="lr_pretrain", type=float,
                         default=5e-1, help='learning rate')
     parser.add_argument("--batch_size_per_gpu_pretrain", dest="batch_size_per_gpu_pretrain", type=int,
@@ -152,6 +154,8 @@ def set_args():
     parser.add_argument("--eval_step_pretrain", dest="eval_step_pretrain", type=int,
                         default=10000, help="how many steps to eval")
     ##### entity prompt tuning
+    parser.add_argument("--optimizer_entity", dest="optimizer_entity", type=str,
+                        default="adafactor", help="optimizer for the entity tuning")
     parser.add_argument("--lr_entity", dest="lr_entity", type=float,
                         default=5e-1, help='learning rate')
     parser.add_argument("--batch_size_per_gpu_entity", dest="batch_size_per_gpu_entity", type=int,
@@ -175,6 +179,8 @@ def set_args():
     parser.add_argument("--eval_step_entity", dest="eval_step_entity", type=int,
                         default=15000, help="how many steps to eval")
     ##### summary prompt tuning
+    parser.add_argument("--optimizer_summary", dest="optimizer_summary", type=str,
+                        default="adafactor", help="optimizer for the summary fine-tuning")
     parser.add_argument("--train_sample_summary", dest="train_sample_summary", type=bool,
                         default=True, help="dynamic sample or not")
     parser.add_argument("--lr_summary", dest="lr_summary", type=float,
@@ -425,7 +431,8 @@ def main(args):
             # check if we have already generated it
             if not os.path.isfile(args.test_file):
                 subsample_2k_testset(dataset_args, args.test_file, args.seed, args)
-            args.test_dataset = T5SummarizationDataset(args.test_file, "valid", args.max_length, tokenizer, allgentasktokens, answertoken, args)
+            args.test_dataset = T5SummarizationDataset(args.test_file, "valid", args.max_length, tokenizer, allgentasktokens, answertoken, args, args.seed,
+                                                        save_path = args.save_dir)
             logger.info(f'args.test_dataset.num_entries: ', args.test_dataset.num_entries)
         print('args.full_testset: ', args.full_testset)
         if args.full_testset:
@@ -556,7 +563,7 @@ def main(args):
                     else:
                         alldata = valid_dataset.data
                         print("valid size: ", len(alldata))
-                        allresofvalid = {}
+                    allresofvalid = {}
                     with torch.no_grad():
                         for step in range(len(alldata)):
                             onedata = alldata[step]
@@ -571,7 +578,7 @@ def main(args):
                             if allentitylist == []:
                                 allentitylist = ["none"]
                             input_guidance = args.separator.join(list(dict.fromkeys(allentitylist)))
-                            print(step, input_guidance)
+                            #print(step, input_guidance)
                             allresofvalid[tempdata] = input_guidance
                     logger.info(len(allresofvalid))
                     with open(respath, "wb") as f:

Summarization/models_summarization/model_soft.py
@@ -1,6 +1,7 @@
 import os
 import pdb
 import sys
+import math
 import torch
 import torch.nn as nn
 
@@ -38,7 +39,7 @@ class ModelSoftPrompt(nn.Module):
     ):
         if 'T5' in self.model_name:
             input_embed_part = self.model.encoder.embed_tokens(input_ids)
-        else:
+        elif "Pegasus" in self.model_name:
             input_embed_part = self.model.get_encoder().embed_tokens(input_ids)
 
         prompt_embed_repeat = self.promptembedding.repeat(input_embed_part.size(0), 1, 1)
@@ -49,6 +50,11 @@ class ModelSoftPrompt(nn.Module):
         else:
             allembedding = torch.cat([prompt_embed_repeat, input_embed_part], 1)
             all_attention_mask = torch.cat([mask_prompt, attention_mask], 1)
+
+        if not(self.args.use_pretrain_ckpt):
+            embed_dim = self.model.config.d_model
+            embed_scale = math.sqrt(embed_dim)
+            allembedding = allembedding * embed_scale
 
         return self.model(
             inputs_embeds=allembedding,
@@ -76,25 +82,32 @@ class ModelSoftPrompt(nn.Module):
     def _generative_step(self, batch):
         if 'T5' in self.model_name:
             input_embed_part = self.model.encoder.embed_tokens(batch["input_ids"])
-        else:
+        elif "Pegasus" in self.model_name:
             input_embed_part = self.model.get_encoder().embed_tokens(batch["input_ids"])
+
         prompt_embed_repeat = self.promptembedding.repeat(input_embed_part.size(0), 1, 1)
         allembedding = torch.cat([input_embed_part, prompt_embed_repeat], 1)
+        if not(self.args.use_pretrain_ckpt):
+            embed_dim = self.model.config.d_model
+            embed_scale = math.sqrt(embed_dim)
+            allembedding = allembedding * embed_scale
+
         mask_prompt = torch.full((batch["attention_mask"].shape[0], self.promptnumber), 1).to(self.args.device)
         all_attention_mask = torch.cat([batch["attention_mask"], mask_prompt], 1)
         decoder_input_ids = (
             torch.ones((batch["input_ids"].shape[0], 1), dtype=torch.long, device=batch["input_ids"].device) * self.decoder_start_token_id_use
         )
+        
         generated_ids = self.model.generate(
             inputs_embeds=allembedding,
             decoder_input_ids=decoder_input_ids,
             attention_mask=all_attention_mask,
-            use_cache=True,
-            num_beams=self.args.num_beams,
-            max_length=self.args.max_summary_length,
-            repetition_penalty=self.args.repetition_penalty,
-            length_penalty=self.args.length_penalty,
-            early_stopping=True
+            max_length = self.args.max_summary_length,
+            num_beams = self.args.num_beams,
+            repetition_penalty = self.args.repetition_penalty,
+            length_penalty = self.args.length_penalty,
+            use_cache = True,
+            early_stopping = True
         )
 
         preds = self.ids_to_clean_text(generated_ids)
@@ -106,10 +119,16 @@ class ModelSoftPrompt(nn.Module):
     def _generative_samples(self, batch):
         if 'T5' in self.model_name:
             input_embed_part = self.model.encoder.embed_tokens(batch["input_ids"])
-        else:
+        elif "Pegasus" in self.model_name:
             input_embed_part = self.model.get_encoder().embed_tokens(batch["input_ids"])
+
         prompt_embed_repeat = self.promptembedding.repeat(input_embed_part.size(0), 1, 1)
         allembedding = torch.cat([input_embed_part, prompt_embed_repeat], 1)
+        if not(self.args.use_pretrain_ckpt):
+            embed_dim = self.model.config.d_model
+            embed_scale = math.sqrt(embed_dim)
+            allembedding = allembedding * embed_scale
+
         mask_prompt = torch.full((batch["attention_mask"].shape[0], self.promptnumber), 1).to(self.args.device)
         all_attention_mask = torch.cat([batch["attention_mask"], mask_prompt], 1)
         decoder_input_ids = (
@@ -120,14 +139,14 @@ class ModelSoftPrompt(nn.Module):
             inputs_embeds=allembedding,
             decoder_input_ids=decoder_input_ids,
             attention_mask=all_attention_mask,
-            use_cache=True,
-            max_length=self.args.max_length,
-            repetition_penalty=2.5,
-            length_penalty=1.0,
-            early_stopping=True,
-            do_sample=True,
+            max_length = self.args.max_length,
+            repetition_penalty = self.args.repetition_penalty,
+            length_penalty = self.args.length_penalty,
+            use_cache = True,
+            early_stopping = True,
+            do_sample = True,
             top_k = 64,
-            num_return_sequences=3
+            num_return_sequences = 3
         )
 
         preds = self.ids_to_clean_text(generated_ids)

Summarization/utils.py
@@ -27,8 +27,7 @@ class Nop(object):
         
 
 def seed_everything(args):
-    seed = args.seed
-    seed = 42
+    seed = int(args.seed)
     random.seed(seed)
     os.environ['PYTHONASSEED'] = str(seed)
     np.random.seed(seed)