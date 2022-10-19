ff0722a04eb07e8ec3fe32a47e72bd1d851745c4
Ravoxsg <mathieu.ravaut@gmail.com>
2022-10-19 17:32:05 +0800

    base

--- Summarization/run_fullshot_baselines.sh
+++ Summarization/run_fullshot_baselines.sh
@@ -1,6 +1,6 @@
 ### dataset
-dataset="xsum" # in ["ccdv/cnn_dailymail", "xsum", "billsum", "samsum"]
-device="7"
+dataset="ccdv/cnn_dailymail" # in ["ccdv/cnn_dailymail", "xsum", "billsum", "samsum"]
+device="1"
 cache='/data/mathieu/hf_models/pegasus-large/'
 
 ### backbone model
@@ -14,12 +14,12 @@ pretrain_prompt_ckpt="/data/mathieu/PromptSumm/t5_tagger_pretrained_ckpt/015_n_4
 
 ########################### Baseline v1: Fine-tuning
 
-##### train & val
+##### train  val
 #echo "start full-shot baseline-1: all-params finetune summary"
-#CUDA_VISIBLE_DEVICES=$device python main_full_shot.py --model PegasusFinetune --dataset_name $dataset --finetune_summary --use_pretrain_ckpt --infer_val_entities --use_entity_chain --use_t5_tagger --if_spacy --model_name google/pegasus-large --use_lm_adapted 0 --cache_path $cache --label_smoothing 0.1
+#CUDA_VISIBLE_DEVICES=$device python main_full_shot.py --model PegasusFinetune --dataset_name $dataset --finetune_summary --use_pretrain_ckpt --infer_val_entities --use_entity_chain --use_t5_tagger --if_spacy --model_name google/pegasus-large --use_lm_adapted 0 --cache_path $cache 
 ##### test
-echo "start full-shot baseline-1: all-params finetune summary - TEST SET"
-CUDA_VISIBLE_DEVICES=$device python main_full_shot.py --model PegasusFinetune --dataset_name $dataset --full_testset --finetune_summary --use_pretrain_ckpt --infer_val_entities --use_entity_chain --use_t5_tagger --if_spacy --max_epoch_summary 0 --model_name google/pegasus-large --use_lm_adapted 0 --cache_path $cache 
+#echo "start full-shot baseline-1: all-params finetune summary - TEST SET"
+#CUDA_VISIBLE_DEVICES=$device python main_full_shot.py --model PegasusFinetune --dataset_name $dataset --full_testset --finetune_summary --use_pretrain_ckpt --infer_val_entities --use_entity_chain --use_t5_tagger --if_spacy --max_epoch_summary 0 --model_name google/pegasus-large --use_lm_adapted 0 --cache_path $cache --test_on_val
 
 ############################ Baseline v2: Soft prompt tuning
 
@@ -27,8 +27,8 @@ CUDA_VISIBLE_DEVICES=$device python main_full_shot.py --model PegasusFinetune --
 #echo "start full-shot baseline-2: simple prompt-tune summary"
 #CUDA_VISIBLE_DEVICES=$device python main_full_shot.py --model PegasusSoftPrompt --dataset_name $dataset --finetune_summary --use_pretrain_ckpt --infer_val_entities --use_entity_chain --use_t5_tagger --if_spacy --model_name google/pegasus-large --use_lm_adapted 0 --cache_path $cache --eval_epoch_0
 ##### test
-#echo "start full-shot baseline-2: simple prompt-tune summary - TEST SET"
-#CUDA_VISIBLE_DEVICES=$device python main_full_shot.py --model PegasusSoftPrompt --dataset_name $dataset --full_testset --finetune_summary --use_pretrain_ckpt --infer_val_entities --use_entity_chain --use_t5_tagger --if_spacy --max_epoch_summary 0 --model_name google/pegasus-large --use_lm_adapted 0 --cache_path $cache
+echo "start full-shot baseline-2: simple prompt-tune summary - TEST SET"
+CUDA_VISIBLE_DEVICES=$device python main_full_shot.py --model PegasusSoftPrompt --dataset_name $dataset --full_testset --finetune_summary --use_pretrain_ckpt --infer_val_entities --use_entity_chain --use_t5_tagger --if_spacy --max_epoch_summary 0 --model_name google/pegasus-large --use_lm_adapted 0 --cache_path $cache
 
 ############################ Baseline v3: Soft prompt tuning from our pre-trained checkpoint
 
@@ -37,4 +37,4 @@ CUDA_VISIBLE_DEVICES=$device python main_full_shot.py --model PegasusFinetune --
 #CUDA_VISIBLE_DEVICES=$device python main_full_shot.py --model PegasusSoftPrompt --dataset_name $dataset --finetune_summary --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt --infer_val_entities --use_entity_chain --use_t5_tagger --if_spacy --model_name google/pegasus-large --use_lm_adapted 0 --cache_path $cache --eval_epoch_0
 ##### test
 #echo "start full-shot baseline-3: simple prompt-tune summary with pretrained ckpt - TEST SET"
-#CUDA_VISIBLE_DEVICES=$device python main_full_shot.py --model PegasusSoftPrompt --dataset_name $dataset --full_testset --finetune_summary --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt --infer_val_entities --use_entity_chain --use_t5_tagger --if_spacy --max_epoch_summary 0 --model_name google/pegasus-large --use_lm_adapted 0  --cache_path $cache
+#CUDA_VISIBLE_DEVICES=$device python main_full_shot.py --model PegasusSoftPrompt --dataset_name $dataset --full_testset --finetune_summary --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt --infer_val_entities --use_entity_chain --use_t5_tagger --if_spacy --max_epoch_summary 0 --model_name google/pegasus-large --use_lm_adapted 0 --cache_path $cache --test_on_val
