84a1016a4327b6d7904155ca634dd864e2b6351f
Ravoxsg <mathieu.ravaut@gmail.com>
2022-12-06 11:47:41 +0800

    base

--- Summarization/run_zeroshot.sh
+++ Summarization/run_zeroshot.sh
@@ -1,7 +1,7 @@
 ### dataset
-dataset="ccdv/cnn_dailymail" # in ["ccdv/cnn_dailymail", "xsum", "billsum", "samsum"]
+dataset="samsum" # in ["ccdv/cnn_dailymail", "xsum", "billsum", "samsum"]
 k_shot="10"
-device="1"
+device="4"
 bs=4
 cache='/data/mathieu/hf_models/pegasus-large/'
 
@@ -31,7 +31,7 @@ pretrain_prompt_ckpt="/data/mathieu/PromptSum/t5_tagger_pretrained_ckpt/015_n_40
 #echo "start 0-shot prompt-tune_entity - TEST SET"
 #CUDA_VISIBLE_DEVICES=$device python main.py --model PegasusMixPrompt --dataset_name $dataset --full_testset --few_shot $k_shot --num_seeds 1 --zero_shot --finetune_entity --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt --max_epoch_entity 0 --max_epoch_summary 0 --model_name google/pegasus-large --use_lm_adapted 0 --cache_path $cache
 echo "start 0-shot prompt-tune_summary - TEST SET"
-CUDA_VISIBLE_DEVICES=$device python main.py --model PegasusMixPrompt --max_test_size 100 --dataset_name $dataset --full_testset --few_shot $k_shot --num_seeds 1 --zero_shot --finetune_summary --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt --max_epoch_summary 0 --model_name google/pegasus-large --use_lm_adapted 0 --cache_path $cache
+CUDA_VISIBLE_DEVICES=$device python main.py --model PegasusMixPrompt --max_test_size 100000 --dataset_name $dataset --full_testset --few_shot $k_shot --num_seeds 1 --zero_shot --finetune_summary --pretrain_ckpt $pretrain_ckpt --pretrain_prompt_ckpt $pretrain_prompt_ckpt --max_epoch_summary 0 --model_name google/pegasus-large --use_lm_adapted 0 --cache_path $cache
 
 ############################ MixPrompt - oracle
 
