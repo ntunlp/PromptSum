da98a57951951caf91ab84aea426f8942034a547
Ravoxsg <mathieu.ravaut@gmail.com>
2023-05-20 17:02:29 +0800

    stat significance in few shot

--- Summarization/check_stat_significance.py
+++ Summarization/check_stat_significance.py
@@ -4,8 +4,9 @@ from scipy.stats import ttest_ind
 
 
 
-datasets = ["xsum", "samsum"]
-volumes = ["full"]
+datasets = ["samsum"]
+volumes = ["100"]
+n_seeds = 3
 metrics = ["r1s", "r2s", "rls", "bs"]
 models = [
     "pretrained_False_oracle_False",
@@ -18,15 +19,39 @@ models = [
 for dataset in datasets:
     for volume in volumes:
         print(f"\nDataset: {dataset}, volume: {volume}")
-        base_scores = pickle.load(open(f"scores/{dataset}/{volume}/prompt_sum_scores_{dataset}_pretrained_True_oracle_False.pkl", "rb"))
-        for model in models:
-            path = f"scores/{dataset}/{volume}/prompt_sum_scores_{dataset}_{model}.pkl"
-            other_scores = pickle.load(open(path, "rb"))
-            print(f"Comparison with model {model}:")
-            for metric in metrics:
-                stat, p_value = ttest_ind(base_scores[metric], other_scores[metric])
-                base = np.mean(base_scores[metric])
-                new = np.mean(other_scores[metric])
-                print(f"Metric {metric}, PromptSum: {base:.4f}, new: {new:.4f}, p-value: {p_value:.8f}")
-
+        if volume == "full":
+            base_scores = pickle.load(open(f"scores/{dataset}/{volume}/prompt_sum_scores_{dataset}_pretrained_True_oracle_False.pkl", "rb"))
+            for model in models:
+                path = f"scores/{dataset}/{volume}/prompt_sum_scores_{dataset}_{model}.pkl"
+                other_scores = pickle.load(open(path, "rb"))
+                print(f"Comparison with model {model}:")
+                for metric in metrics:
+                    stat, p_value = ttest_ind(base_scores[metric], other_scores[metric])
+                    base = np.mean(base_scores[metric])
+                    new = np.mean(other_scores[metric])
+                    print(f"Metric {metric}, PromptSum: {base:.4f}, new: {new:.4f}, p-value: {p_value:.8f}")
+        else:
+            all_base_scores = []
+            for j in range(n_seeds):
+                base_scores = pickle.load(open(f"scores/{dataset}/{volume}/prompt_sum_scores_{dataset}_seed_{j}_pretrained_True_oracle_False.pkl", "rb"))
+                all_base_scores.append(base_scores)
+            base_scores = {}
+            for k in all_base_scores[0].keys():
+                base_scores[k] = np.mean(np.concatenate([np.expand_dims(x[k], 0) for x in all_base_scores]), 0)
+            for model in models:
+                all_other_scores = []
+                for j in range(n_seeds):
+                    path = f"scores/{dataset}/{volume}/prompt_sum_scores_{dataset}_seed_{j}_{model}.pkl"
+                    other_scores = pickle.load(open(path, "rb"))
+                    all_other_scores.append(other_scores)
+                other_scores = {}
+                for k in all_other_scores[0].keys():
+                    other_scores[k] = np.mean(np.concatenate([np.expand_dims(x[k], 0) for x in all_other_scores]), 0)
+                print(f"Comparison with model {model}:")
+                for metric in metrics:
+                    stat, p_value = ttest_ind(base_scores[metric], other_scores[metric])
+                    base = np.mean(base_scores[metric])
+                    new = np.mean(other_scores[metric])
+                    print(f"Metric {metric}, PromptSum: {base:.4f}, new: {new:.4f}, p-value: {p_value:.8f}")
+    
 
